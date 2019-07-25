584e607ab1ffce7d33b0d8cefe87b539995a205a
David Gurney <gurneyds@familysearch.org>
2019-07-24 19:59:56 -0600

    Preparing to add auth

--- src/App.js
+++ src/App.js
@@ -1,56 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
-      </div>
-    </Router>
+    <CssBaseline>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
+    </CssBaseline>
   );
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2>Home</h2>
+      <Button variant="contained" color="primary">
+        Material button
+      </Button>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <React.Fragment>
+      <Header />
+
+      <div>
+        <h2>Dashboard page</h2>
+      </div>
+    </React.Fragment>
+  );
 }
 
 function About() {
-  return <h2>About</h2>;
+  return (
+    <React.Fragment>
+      <Header />
+
+      <h2>About</h2>
+    </React.Fragment>
+  );
 }
 
 function Topic({ match }) {
-  return <h3>Requested Param: {match.params.id}</h3>;
+  return (
+    <React.Fragment>
+      <Header />
+
+      <h3>Requested Param: {match.params.id}</h3>
+    </React.Fragment>
+  );
 }
 
 function Topics({ match }) {
   return (
-    <div>
-      <h2>Topics</h2>
+    <React.Fragment>
+      <Header />
 
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
+      <div>
+        <h2>Topics</h2>
 
-      <Route path={`${match.path}/:id`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
+        <ul>
+          <li>
+            <Link to={`${match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+          </li>
+        </ul>
+
+        <Route path={`${match.path}/:id`} component={Topic} />
+        <Route
+          exact
+          path={match.path}
+          render={() => <h3>Please select a topic.</h3>}
+        />
+      </div>
+    </React.Fragment>
   );
 }
 
@@ -59,6 +96,9 @@ function Header() {
     <ul className="Header">
       <li>
         <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
       </li>
       <li>
         <Link to="/about">About</Link>
